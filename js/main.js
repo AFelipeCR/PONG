74ec3953201e35ed77605a5a7b66707ec52defa0
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2020-06-15 17:36:09 -0500

    Validacion fuerte, y boton stop

--- js/main.js
+++ js/main.js
@@ -9,18 +9,21 @@ var inMouse = false;
 var scoreA = 0;
 var scoreB = 0;
 var limitScore=0;
+var maxScore=21;
+var minScore=3;
+var isStopped=false;
 var canvasPong = document.getElementById("canvas_pong");
 
-var pong = new Pong(document, canvasPong, 0, 0);
+var pong;
 
 function draw() {
     pong.draw();
    
-
-    if(scoreA==limitScore || scoreB==limitScore){
+    if(scoreA==limitScore || scoreB==limitScore || isStopped){
     	cancelAnimationFrame(draw);
     	pong.showGameOver();
-    	document.getElementById('start-button').disabled=false;
+    	document.getElementById('start-button').hidden=false;
+        document.getElementById('stop-button').hidden=true;
     	document.getElementById('limit-score').disabled=false;
     }else{
     	requestAnimationFrame(draw);
@@ -28,8 +31,11 @@ function draw() {
 }
 
 function startGame(){
+    pong = new Pong(document, canvasPong, 0, 0);
 	scoreA=scoreB=0;
-	document.getElementById('start-button').disabled=true;
+    isStopped=false;
+    document.getElementById('start-button').hidden=true;
+    document.getElementById('stop-button').hidden=false;
 	document.getElementById('limit-score').disabled=true;
 	limitScore = document.getElementById('limit-score').value;
 	draw();
